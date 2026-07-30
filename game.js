0c925bc56915ab0d6c60b85a1dd8a701be67c229
siglaboges <siglaboges@gmail.com>
2026-07-30 20:57:10 +0800

    Update game.js

--- game.js
+++ game.js
@@ -5,45 +5,115 @@ canvas.width = 900;
 canvas.height = 600;
 
 
-// Test enemy
+// Enemy image
+const enemyImg = new Image();
+enemyImg.src = "sprite/enemy.png";
+
+
+// Enemy
 let enemy = {
-    x: 50,
+    x: 0,
     y: 200,
-    speed: 2
+    speed: 2,
+    point: 1
 };
 
 
-function gameLoop(){
+// Path
+const path = [
+    {x:0, y:200},
+    {x:600, y:200},
+    {x:600, y:450},
+    {x:900, y:450}
+];
 
-    // background
+
+
+function drawMap(){
+
+    // grass
     ctx.fillStyle = "#6ac34a";
     ctx.fillRect(0,0,900,600);
 
 
-    // path
+    // dirt path
     ctx.strokeStyle = "#b88652";
     ctx.lineWidth = 80;
+    ctx.lineCap = "round";
 
     ctx.beginPath();
-    ctx.moveTo(0,200);
-    ctx.lineTo(600,200);
-    ctx.lineTo(600,450);
-    ctx.lineTo(900,450);
+
+    ctx.moveTo(path[0].x,path[0].y);
+
+    for(let i = 1; i < path.length; i++){
+        ctx.lineTo(path[i].x,path[i].y);
+    }
+
     ctx.stroke();
 
-
-    // move enemy
-    enemy.x += enemy.speed;
+}
 
 
-    // draw enemy (red square test)
-    ctx.fillStyle = "red";
-    ctx.fillRect(
-        enemy.x,
-        enemy.y,
+
+function moveEnemy(){
+
+    let target = path[enemy.point];
+
+
+    let dx = target.x - enemy.x;
+    let dy = target.y - enemy.y;
+
+    let distance = Math.sqrt(dx*dx + dy*dy);
+
+
+    if(distance < enemy.speed){
+
+        enemy.x = target.x;
+        enemy.y = target.y;
+
+        enemy.point++;
+
+        if(enemy.point >= path.length){
+
+            enemy.x = 0;
+            enemy.y = 200;
+            enemy.point = 1;
+
+        }
+
+    }
+    else {
+
+        enemy.x += (dx/distance) * enemy.speed;
+        enemy.y += (dy/distance) * enemy.speed;
+
+    }
+
+}
+
+
+
+function drawEnemy(){
+
+    ctx.drawImage(
+        enemyImg,
+        enemy.x-25,
+        enemy.y-25,
         50,
         50
     );
+
+}
+
+
+
+function gameLoop(){
+
+    drawMap();
+
+    moveEnemy();
+
+    drawEnemy();
 
 
     requestAnimationFrame(gameLoop);
