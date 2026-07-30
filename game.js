b21e1b2111963cece87433bf81bfa2ea8b15b630
siglaboges <siglaboges@gmail.com>
2026-07-30 22:32:51 +0800

    Update game.js

--- game.js
+++ game.js
@@ -13,6 +13,10 @@ const enemyImg = new Image();
 enemyImg.src = "sprite/enemy.png";
 
 
+const arrowImg = new Image();
+arrowImg.src = "sprite/arrow.png";
+
+
 // =================
 // PLAYER
 // =================
@@ -23,6 +27,21 @@ let lives = 100;
 let stage = 1;
 
 let gameOver = false;
+
+
+// =================
+// SHOP
+// =================
+
+let selectedTower = null;
+
+
+// =================
+// OBJECTS
+// =================
+
+let enemies = [];
+let towers = [];
 
 
 // =================
@@ -38,11 +57,8 @@ const path = [
 
 
 // =================
-// ENEMIES
+// SPAWN ENEMIES
 // =================
-
-let enemies = [];
-
 
 function spawnEnemy(){
 
@@ -63,15 +79,85 @@ function spawnEnemy(){
 }
 
 
-// Spawn enemies every second
-
 setInterval(()=>{
 
     if(!gameOver){
+
         spawnEnemy();
+
     }
 
 },1000);
+
+
+
+// =================
+// PLACE TOWER
+// =================
+
+canvas.addEventListener("click", function(event){
+
+
+    if(gameOver) return;
+
+
+    let rect = canvas.getBoundingClientRect();
+
+
+    let mouseX = event.clientX - rect.left;
+    let mouseY = event.clientY - rect.top;
+
+
+
+    // SHOP CLICK
+
+    if(
+        mouseX >= 10 &&
+        mouseX <= 110 &&
+        mouseY >= 500 &&
+        mouseY <= 590
+    ){
+
+        selectedTower = "arrow";
+
+        return;
+
+    }
+
+
+
+    // PLACE ARROW
+
+    if(selectedTower === "arrow"){
+
+
+        if(coins >= 100){
+
+
+            towers.push({
+
+                x:mouseX,
+                y:mouseY,
+
+                range:200,
+
+                cooldown:0
+
+            });
+
+
+            coins -= 100;
+
+
+        }
+
+
+        selectedTower=null;
+
+    }
+
+
+});
 
 
 
@@ -105,6 +191,7 @@ function moveEnemies(){
             enemy.x=target.x;
             enemy.y=target.y;
 
+
             enemy.point++;
 
 
@@ -118,6 +205,7 @@ function moveEnemies(){
                 enemies.splice(index,1);
 
 
+
                 if(lives<=0){
 
                     lives=0;
@@ -125,7 +213,9 @@ function moveEnemies(){
 
                 }
 
+
             }
+
 
 
         }
@@ -133,13 +223,17 @@ function moveEnemies(){
 
 
             enemy.x += (dx/distance)*enemy.speed;
+
             enemy.y += (dy/distance)*enemy.speed;
 
 
         }
 
 
+
     });
+
+
 
 }
 
@@ -156,38 +250,142 @@ function drawEnemies(){
 
 
         ctx.drawImage(
+
             enemyImg,
+
             enemy.x-25,
+
             enemy.y-25,
+
             50,
+
             50
+
         );
 
 
 
-        // HP bar
+        // HP BAR
 
         ctx.fillStyle="black";
 
         ctx.fillRect(
+
             enemy.x-25,
+
             enemy.y-40,
+
             50,
+
             7
+
         );
 
 
         ctx.fillStyle="red";
 
         ctx.fillRect(
+
             enemy.x-25,
+
             enemy.y-40,
+
             50*(enemy.hp/enemy.maxHp),
+
             7
+
         );
 
 
     });
+
+
+}
+
+
+
+// =================
+// DRAW TOWERS
+// =================
+
+function drawTowers(){
+
+
+    towers.forEach(tower=>{
+
+
+        ctx.drawImage(
+
+            arrowImg,
+
+            tower.x-25,
+
+            tower.y-25,
+
+            50,
+
+            50
+
+        );
+
+
+    });
+
+
+}
+
+
+
+// =================
+// MAP
+// =================
+
+function drawMap(){
+
+
+    ctx.fillStyle="#6ac34a";
+
+    ctx.fillRect(
+        0,
+        0,
+        900,
+        600
+    );
+
+
+
+    ctx.strokeStyle="#b88652";
+
+    ctx.lineWidth=80;
+
+    ctx.lineCap="round";
+
+
+
+    ctx.beginPath();
+
+
+    ctx.moveTo(
+        path[0].x,
+        path[0].y
+    );
+
+
+
+    for(let i=1;i<path.length;i++){
+
+
+        ctx.lineTo(
+            path[i].x,
+            path[i].y
+        );
+
+
+    }
+
+
+
+    ctx.stroke();
 
 
 }
@@ -200,24 +398,41 @@ function drawEnemies(){
 
 function drawUI(){
 
+
     ctx.fillStyle="black";
-    ctx.fillRect(0,0,900,50);
+
+    ctx.fillRect(
+        0,
+        0,
+        900,
+        50
+    );
+
 
 
     ctx.fillStyle="white";
+
     ctx.font="20px Arial";
 
 
+
     ctx.fillText("$"+coins,20,32);
+
     ctx.fillText("💎 "+gems,150,32);
+
     ctx.fillText("❤️ "+lives,280,32);
+
     ctx.fillText("Stage "+stage,450,32);
+
 
 
     if(gameOver){
 
+
         ctx.fillStyle="red";
+
         ctx.font="60px Arial";
+
 
         ctx.fillText(
             "GAME OVER",
@@ -225,43 +440,79 @@ function drawUI(){
             150
         );
 
+
     }
+
 
 }
 
 
 
 // =================
-// MAP
+// SHOP
 // =================
 
-function drawMap(){
-
-    ctx.fillStyle="#6ac34a";
-    ctx.fillRect(0,0,900,600);
+function drawShop(){
 
 
-    ctx.strokeStyle="#b88652";
-    ctx.lineWidth=80;
-    ctx.lineCap="round";
+    ctx.fillStyle="#333";
+
+    ctx.fillRect(
+        10,
+        500,
+        100,
+        90
+    );
 
 
-    ctx.beginPath();
 
-    ctx.moveTo(path[0].x,path[0].y);
+    ctx.drawImage(
+
+        arrowImg,
+
+        35,
+
+        510,
+
+        50,
+
+        50
+
+    );
 
 
-    for(let i=1;i<path.length;i++){
 
-        ctx.lineTo(
-            path[i].x,
-            path[i].y
+    ctx.fillStyle="white";
+
+    ctx.font="italic 20px cursive";
+
+
+    ctx.fillText(
+        "100$",
+        30,
+        585
+    );
+
+
+
+    if(selectedTower==="arrow"){
+
+
+        ctx.strokeStyle="yellow";
+
+        ctx.lineWidth=3;
+
+
+        ctx.strokeRect(
+            10,
+            500,
+            100,
+            90
         );
+
 
     }
 
-
-    ctx.stroke();
 
 }
 
@@ -273,16 +524,28 @@ function drawMap(){
 
 function gameLoop(){
 
+
     drawMap();
+
 
     moveEnemies();
 
+
+    drawTowers();
+
+
     drawEnemies();
+
 
     drawUI();
 
 
+    drawShop();
+
+
+
     requestAnimationFrame(gameLoop);
+
 
 }
 
