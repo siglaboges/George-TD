7c34ef5787409b2752f3065c3cfbbcdf13f8b5e0
siglaboges <siglaboges@gmail.com>
2026-07-31 20:17:47 +0800

    Update enemyBook.js

--- enemyBook.js
+++ enemyBook.js
@@ -50,19 +50,19 @@ var enemyBook = {
 
     7:{
         hp:100,
-        split:"Tier 1 + Tier 6"
+        split:"1 Tier 1 + 1 Tier 6"
     },
 
 
     8:{
         hp:300,
-        split:"Tier 2 + Tier 7"
+        split:"1 Tier 2 + 1 Tier 7"
     },
 
 
     9:{
         hp:750,
-        split:"Tier 1 + Tier 8"
+        split:"1 Tier 1 + 1 Tier 8"
     }
 
 
@@ -88,7 +88,7 @@ function drawEnemyMenu(){
 
 
 
-    // MENU BACKGROUND
+    // BACKGROUND
 
     ctx.fillStyle =
     "rgba(0,0,0,0.85)";
@@ -140,7 +140,7 @@ function drawEnemyMenu(){
 
 
 
-    // CONTENT
+    // ENEMY LIST
 
     ctx.font="20px Arial";
 
