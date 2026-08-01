c467d335ee5ab0166e880f186fb9be9f1a01ac27
siglaboges <siglaboges@gmail.com>
2026-08-01 10:52:02 +0800

    Update towers.js

--- towers.js
+++ towers.js
@@ -1,443 +1,5 @@
 // =========================
-// GEORGE TD v0.4.4.2
-// TOWERS + PROJECTILES
-// =========================
-
-
-// =========================
-// PLACE TOWER
-// =========================
-
-function placeTower(x,y){
-
-
-    towers.push({
-
-        x:x,
-
-        y:y,
-
-        damage:1,
-
-        range:220,
-
-        cooldown:0,
-
-        fireRate:40,
-
-        angle:0
-
-    });
-
-
-    coins-=100;
-
-
-}
-
-
-
-
-
-
-// =========================
-// UPDATE TOWERS
-// =========================
-
-function updateTowers(){
-
-
-    towers.forEach(function(tower){
-
-
-        if(tower.cooldown>0){
-
-            tower.cooldown--;
-
-        }
-
-
-
-        var target=null;
-
-
-
-        enemies.forEach(function(enemy){
-
-
-            var distance=Math.sqrt(
-
-                (enemy.x-tower.x)**2+
-                (enemy.y-tower.y)**2
-
-            );
-
-
-
-            if(distance<tower.range){
-
-                target=enemy;
-
-            }
-
-
-        });
-
-
-
-
-
-        if(target){
-
-
-            tower.angle=Math.atan2(
-
-                target.y-tower.y,
-
-                target.x-tower.x
-
-            );
-
-
-
-
-
-            if(tower.cooldown<=0){
-
-
-
-                projectiles.push({
-
-
-                    x:tower.x,
-
-                    y:tower.y,
-
-
-                    target:target,
-
-
-                    speed:8,
-
-
-                    damage:tower.damage,
-
-
-                    angle:tower.angle
-
-
-                });
-
-
-
-                tower.cooldown=tower.fireRate;
-
-
-
-            }
-
-
-
-        }
-
-
-
-    });
-
-
-
-}
-
-
-
-
-
-
-
-
-// =========================
-// UPDATE PROJECTILES
-// =========================
-
-function updateProjectiles(){
-
-
-
-    projectiles.forEach(function(arrow,index){
-
-
-
-        if(!arrow.target){
-
-
-            projectiles.splice(index,1);
-
-
-            return;
-
-
-        }
-
-
-
-
-
-        var dx =
-        arrow.target.x-arrow.x;
-
-
-
-        var dy =
-        arrow.target.y-arrow.y;
-
-
-
-        var distance=Math.sqrt(
-
-            dx*dx+
-            dy*dy
-
-        );
-
-
-
-
-
-        arrow.angle=Math.atan2(
-
-            dy,
-
-            dx
-
-        );
-
-
-
-
-
-
-
-        if(distance<arrow.speed){
-
-
-
-            arrow.target.hp-=arrow.damage;
-
-
-
-            projectiles.splice(index,1);
-
-
-
-
-
-            if(arrow.target.hp<=0){
-
-
-
-                var enemyIndex =
-                enemies.indexOf(
-                    arrow.target
-                );
-
-
-
-                if(enemyIndex!=-1){
-
-
-
-                    enemies.splice(
-
-                        enemyIndex,
-
-                        1
-
-                    );
-
-
-
-                    coins+=10;
-
-
-                }
-
-
-
-            }
-
-
-
-        }
-        else{
-
-
-            arrow.x +=
-            (dx/distance)
-            *
-            arrow.speed;
-
-
-
-            arrow.y +=
-            (dy/distance)
-            *
-            arrow.speed;
-
-
-
-        }
-
-
-
-    });
-
-
-
-}
-
-
-
-
-
-
-
-
-// =========================
-// DRAW TOWERS
-// =========================
-
-function drawTowers(){
-
-
-
-    towers.forEach(function(tower){
-
-
-
-        ctx.save();
-
-
-
-        ctx.translate(
-
-            tower.x,
-
-            tower.y
-
-        );
-
-
-
-        ctx.rotate(
-
-            tower.angle-Math.PI/2
-
-        );
-
-
-
-        ctx.drawImage(
-
-            arrowImg,
-
-            -25,
-
-            -25,
-
-            50,
-
-            50
-
-        );
-
-
-
-        ctx.restore();
-
-
-
-    });
-
-
-
-}
-
-
-
-
-
-
-
-
-// =========================
-// DRAW PROJECTILES
-// =========================
-
-function drawProjectiles(){
-
-
-
-    projectiles.forEach(function(arrow){
-
-
-
-        ctx.save();
-
-
-
-        ctx.translate(
-
-            arrow.x,
-
-            arrow.y
-
-        );
-
-
-
-        ctx.rotate(
-
-            arrow.angle-Math.PI/2
-
-        );
-
-
-
-        ctx.drawImage(
-
-            arrowPImg,
-
-            -17.5,
-
-            -17.5,
-
-            35,
-
-            35
-
-        );
-
-
-
-        ctx.restore();
-
-
-
-    });
-
-
-
-}
-
-
-
-
-
-
-
-
-// =========================
-// RANGE CIRCLES
+// RANGE CIRCLES v0.4.4.2
 // =========================
 
 function drawRangeCircles(){
@@ -448,12 +10,11 @@ function drawRangeCircles(){
     // PLACING TOWER
     // =====================
 
-
     if(selectedTower==="arrow"){
 
 
 
-        // Existing tower spacing circles
+        // Show forbidden zones around towers
 
         towers.forEach(function(tower){
 
@@ -469,7 +30,7 @@ function drawRangeCircles(){
 
                 tower.y,
 
-                40,
+                20,
 
                 0,
 
@@ -481,7 +42,6 @@ function drawRangeCircles(){
 
             ctx.strokeStyle="white";
 
-
             ctx.lineWidth=3;
 
 
@@ -490,7 +50,6 @@ function drawRangeCircles(){
 
 
         });
-
 
 
 
@@ -513,9 +72,7 @@ function drawRangeCircles(){
 
 
 
-
-        // New tower attack range preview
-
+        // Attack range preview
 
         ctx.beginPath();
 
@@ -534,6 +91,8 @@ function drawRangeCircles(){
             Math.PI*2
 
         );
+
+
 
 
 
@@ -577,8 +136,8 @@ function drawRangeCircles(){
 
 
 
-        // Placement collision circle
 
+        // Placement collision circle
 
         ctx.beginPath();
 
@@ -590,7 +149,7 @@ function drawRangeCircles(){
 
             mouseY,
 
-            40,
+            20,
 
             0,
 
@@ -600,7 +159,7 @@ function drawRangeCircles(){
 
 
 
-        ctx.strokeStyle=
+        ctx.strokeStyle =
 
         invalidPlacement
 
@@ -611,6 +170,8 @@ function drawRangeCircles(){
 
 
         ctx.stroke();
+
+
 
 
 
@@ -638,6 +199,7 @@ function drawRangeCircles(){
         var distance=Math.sqrt(
 
             (mouseX-tower.x)**2+
+
             (mouseY-tower.y)**2
 
         );
@@ -669,6 +231,7 @@ function drawRangeCircles(){
 
 
             ctx.fillStyle=
+
             "rgba(0,150,255,0.5)";
 
 
